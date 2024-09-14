b2d402ad400e6703002f77d2c7c2743e4b7ccd34
홍정민 <dream7739@naver.com>
2024-09-14 13:15:20 +0900

    Initial Commit

---
+++ Rebit/RebitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebitApp.swift
+//  Rebit
+//
+//  Created by 홍정민 on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct RebitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
